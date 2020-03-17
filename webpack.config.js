dd61ec3d52bbe6cc4d4a1b2ff0d4cc4c075aa1ea
Mikołaj Borzyszkowski <mikolaj@borzyszkowski.org>
2020-03-17 21:04:48 +0100

    packages updated, nodemon plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const NodemonPlugin = require('nodemon-webpack-plugin');
 
 const buildDir = "build";
 
@@ -34,8 +35,7 @@ module.exports = {
                             modules: true,
                             importLoaders: 1,
                             localIdentName: "[name]_[local]_[hash:base64]",
-                            sourceMap: true,
-                            minimize: true
+                            sourceMap: true
                         }
                     }
                 ]
@@ -51,6 +51,12 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({template: './client/public/index.html'})
+        new HtmlWebpackPlugin({
+            template: './client/public/index.html',
+            favicon: './client/public/favicon.ico'
+        }),
+        new NodemonPlugin({
+            script: './server/index.js'
+        })
     ]
 };
