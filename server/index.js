c3125a2be2ee6a428c05bef4fa12decfd4e6fb91
Mikołaj Borzyszkowski <mikolaj@borzyszkowski.org>
2020-03-17 01:27:12 +0100

    backend mockup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-// Backend endpoint
+const express = require('express');
+const os = require('os');
+
+const app = express();
+
+app.use(express.static('build'));
+app.get('/api/search/:query', (req, res) => res.send({ 
+    pictures: 'Will be served here',
+    query: req.params.query
+}));
+app.listen(8080, () => console.log('Server started!'));
