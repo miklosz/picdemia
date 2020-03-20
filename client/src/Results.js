2c25ef1827c849b3c6a0f6e5f6f3df9cc1802f93
Mikołaj Borzyszkowski <mikolaj@borzyszkowski.org>
2020-03-20 13:54:34 +0100

    details on result list

--- client/src/Results.js
+++ client/src/Results.js
@@ -2,21 +2,25 @@ import React from 'react';
 import styles from './Results.module.css'
 
 function ListItem(props) {
-    const {full,thumbnail,title,pageUrl,service} = props.item;
+    const { full, thumbnail, title, pageUrl, service } = props.item;
+    console.log(full);
     return (
         <li>
-            {/* todo: overlay on hover with link to pixabay / giphny and "save image" link */}
-            <a href={pageUrl} target="_blank">
+            <a href={pageUrl} target="_blank" download={service}>
                 <img src={thumbnail} alt={`${title} from ${service}`} />
+                <h4 className={styles.detailsTop}>{title}</h4>
+                {/* <div className={styles.detailsBottom}>
+                    add link to download
+                </div> */}
             </a>
         </li>
     )
-} 
-    
+}
+
 export default function Results(props) {
     let empty = props.results.length === 0;
 
-    return(
+    return (
         <div className={styles.results}>
             <h2 className={styles.empty}>{empty && 'No '} results for: {props.query}</h2>
             <ul className={styles.ul}>
@@ -25,5 +29,5 @@ export default function Results(props) {
                 )}
             </ul>
         </div>
-    ) 
+    )
 }    
